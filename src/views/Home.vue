2d4d76b995abdb9b5e0305a82be4181aaf6bcbd6
myjdml <myjdml_codemaster@163.com>
2020-10-15 17:21:20 +0800

    feat(game'): t

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,7 +24,7 @@ export default {
   methods: {
     Start () {
       this.$router.push('/game')
-      // this.getGrant()
+      this.getGrant()
     },
     getGrant () {
       if (this.is_ios()) {
