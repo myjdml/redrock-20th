e9c4ebfe2af3cf9fb58006b3eecaadd2168cedb0
myjdml <myjdml_codemaster@163.com>
2020-10-17 19:46:04 +0800

    fix(ajsx): formdata

--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,17 @@ export const instance = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? 'https://cyxbsmobile.redrock.team/wxapi/redrock-20th-anniversary/' : '/api',
   timeout: 50000,
   headers: {
-    'Content-Type': 'multipart/form-data'
-  }
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  transformRequest: [
+    function (data) {
+      let ret = ''
+      for (const it in data) {
+        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+      }
+      return ret
+    }
+  ]
 })
 export async function postWorkLessInfo (url, formValue) {
   return await instance.post(url, {
