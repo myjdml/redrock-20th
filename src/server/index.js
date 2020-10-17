c07bd84bc623a48aa12fbf569712334d964cc4e8
myjdml <myjdml_codemaster@163.com>
2020-10-17 20:24:55 +0800

    fix(ajax): t

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ export const instance = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? 'https://cyxbsmobile.redrock.team/wxapi/redrock-20th-anniversary/' : '/api',
   timeout: 50000,
   headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
+    'Content-Type': 'multipart/form-data'
   },
   transformRequest: [
     function (data) {
