45b1b93d368600db3d18ce533646c732bd0a0a5a
myjdml <myjdml_codemaster@163.com>
2020-10-26 15:48:00 +0800

    'fix(game):ball'

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -97,9 +97,9 @@ export default {
         const tofix = num => (num ? Math.abs(num) / num : 0)
         const GY = Math.abs(obj.b) < 10 || Math.abs(obj.b) > 170 ? 0 : obj.b
         const GX = Math.abs(obj.g) < 10 ? 0 : obj.g
-        console.log('GX' + -tofix(GX) + 'GY' + tofix(GY))
+        // console.log('GX' + -tofix(GX) + 'GY' + tofix(GY))
         e.x = tofix(GX)
-        e.y = tofix(GY) < 0 ? 0 : tofix(GY)
+        e.y = tofix(GY) < 0 ? tofix(GY) * 0.4 : tofix(GY)
       }
       // console.log(o.onOrient)
       o.on()
