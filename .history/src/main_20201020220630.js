06830d921292fe8c8d2226db56bd35392a9ee1a1
myjdml <myjdml_codemaster@163.com>
2020-11-02 09:08:03 +0800

    fix(main): 取消挂载v-console

---
+++ .history/src/main_20201020220630.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'normalize.css'
+import '@/assets/style/font.css'
+import VConsole from 'vconsole'
+/* eslint-disable no-unused-vars */
+createApp(App).use(store).use(router).mount('#app')
+const vConsole = new VConsole() // 初始化
--- .history/src/main_20201102090703.js
+++ .history/src/main_20201102090703.js
@@ -1,11 +1,3 @@
-/*
- * @Author: Code_Master
- * @Date: 2020-10-02 15:21:05
- * @LastEditTime: 2020-11-02 09:07:04
- * @LastEditors: Code_Master
- * @FilePath: /redrock-20th/src/main.js
- * @Description: The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.
- */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
